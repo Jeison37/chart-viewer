95f613dafb7b7424f2e1e9a31475a967db925eb3
Jeison37 <150383172+Jeison37@users.noreply.github.com>
2026-06-05 16:35:13 -0400

    Desarrollar test_load_data_success y test_load_data_failure

--- test_csv_reader.py
+++ test_csv_reader.py
@@ -2,7 +2,20 @@ import unittest
 from csvreader import CSVReader 
 
 class TestCSVReader(unittest.TestCase):
-    pass
+    
+    def test_load_data_success(self):
+        reader = CSVReader()
+        res = reader.load_data("datos.csv")
+        
+        self.assertTrue(res)
+        self.assertIsNotNone(reader._raw_data)
+
+    def test_load_data_failure(self):
+        reader = CSVReader()
+        res = reader.load_data("fail_test.csv")
+        
+        self.assertFalse(res)
+        self.assertIsNone(reader._raw_data)
 
 if __name__ == "__main__":
     unittest.main()
